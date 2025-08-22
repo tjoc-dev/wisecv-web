403c746d6864c3c16c175676c739b3dbcd5fc06c
Joseph Olaoye <olaoye.joseph@gmail.com>
2025-08-22 20:23:44 +0100

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDQeasROFvIc3g2NjMf6OaSfW4rjPaYIvU",
-    authDomain: "tjoc-60df5.firebaseapp.com",
-    projectId: "tjoc-60df5",
-    storageBucket: "tjoc-60df5.firebasestorage.app",
-    messagingSenderId: "1094980532590",
-    appId: "1:1094980532590:web:47a54b343169cd5360697f",
-    measurementId: "G-Y4Z9MRXDE6"
+    apiKey: "****",
+    authDomain: "****",
+    projectId: "****",
+    storageBucket: "****",
+    messagingSenderId: "*****",
+    appId: "******",
+    measurementId: "*****"
 });
 
 const messaging = firebase.messaging();
